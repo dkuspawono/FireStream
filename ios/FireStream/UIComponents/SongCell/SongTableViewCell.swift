a3e37b6b4c23ff8f11d1fcf2ad2dd3455c3c6e67
Caleb Adcock <caleb.m.adcock@gmail.com>
2016-11-19 11:50:02 -0500

    Initial iOS Commit

---
+++ ios/FireStream/UIComponents/SongCell/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  FireStream
+//
+//  Created by Caleb Adcock on 11/19/16.
+//  Copyright © 2016 Syntonic LLC. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
